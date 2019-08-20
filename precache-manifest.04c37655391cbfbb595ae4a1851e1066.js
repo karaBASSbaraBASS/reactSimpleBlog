636786fe912686627538368c6f1607c886c7a03a
Jack <evgen.mikhalov@gmail.com>
2019-08-21 00:05:19 +0300

    Updates

---
+++ precache-manifest.04c37655391cbfbb595ae4a1851e1066.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "675f579268254b18357bc567c47e6a8f",
+    "url": "/reactSimpleBlog/index.html"
+  },
+  {
+    "revision": "7902548a4b30189c0589",
+    "url": "/reactSimpleBlog/static/css/main.dc7f18fe.chunk.css"
+  },
+  {
+    "revision": "7589cbdc93a1af09f574",
+    "url": "/reactSimpleBlog/static/js/2.1fc2548f.chunk.js"
+  },
+  {
+    "revision": "7902548a4b30189c0589",
+    "url": "/reactSimpleBlog/static/js/main.a5a5fe58.chunk.js"
+  },
+  {
+    "revision": "b45f2a995cad40368f8a",
+    "url": "/reactSimpleBlog/static/js/runtime~main.970fcbc9.js"
+  },
+  {
+    "revision": "e6bce08bda084c2869f370ecc4bdf876",
+    "url": "/reactSimpleBlog/static/media/data-random-squares.e6bce08b.svg"
+  },
+  {
+    "revision": "6cd898e0465208c6a5587f01407f6898",
+    "url": "/reactSimpleBlog/static/media/facebook-logo.6cd898e0.svg"
+  },
+  {
+    "revision": "652a3a5154c052674775ac13b1f654d0",
+    "url": "/reactSimpleBlog/static/media/instagram.652a3a51.svg"
+  },
+  {
+    "revision": "0fd0f820709c36ee9d70a3524d00fe96",
+    "url": "/reactSimpleBlog/static/media/twitter.0fd0f820.svg"
+  }
+]);
